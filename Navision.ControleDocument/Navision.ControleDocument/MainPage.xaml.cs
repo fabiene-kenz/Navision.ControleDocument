9b84af5e2a0e9e8124e0252c1cf67ab5f26d419c
Fabien <fabien.richard@hotmail.fr>
2018-05-02 09:35:42 +0200

    Ajout exe pour création json

---
+++ Navision.ControleDocument/Navision.ControleDocument/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Navision.ControleDocument
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
