27332a2313d34c40ab88d5a1280e28995b9332e7
loriscalisti <loris.calisti@e-kenz.com>
2018-05-03 14:17:21 +0200

    - Création du Model Company - Création et récupération de la liste des entreprises - Serialisation de la liste des entreprises en objet JSON - Ecriture du JSON dans le fichier test.json (Services)

---
+++ Creation Json/Model/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creation_Json.Model
+{
+    public class Company
+    {
+        public string CompanyName { get; set; }
+        public string Url { get; set; }
+    }
+}
--- Creation Json/Program.cs
+++ Creation Json/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using Creation_Json.Model;
+using Newtonsoft.Json;
 
 namespace Creation_Json
 {
@@ -10,6 +14,53 @@ namespace Creation_Json
     {
         static void Main(string[] args)
         {
+            List<Company> list = getCompanyList();
+            string jsonString = serializeList(list);
+            writeStringToFile(jsonString);
+            Console.ReadLine();
+        }
+
+        static private List<Company> getCompanyList()
+        {
+            Console.WriteLine("Recuperation de la list des Companies");
+            List<Company> list = new List<Company>();
+
+            list.Add(new Company() {CompanyName = "Company", Url="URL"});
+            list.Add(new Company() {CompanyName = "Company1", Url = "URL1"});
+            list.Add(new Company() { CompanyName = "Company2", Url = "URL2" });
+
+            return list;
+        }
+
+        static private string serializeList(List<Company> companyList)
+        {
+            Console.WriteLine("Serialisation de la liste des companies");
+            try
+            {
+                string jsonString = JsonConvert.SerializeObject(companyList, Formatting.Indented);
+                Console.WriteLine(jsonString);
+                return jsonString;
+
+            }
+            catch (JsonException e)
+            {
+                throw e;
+            }
+        }
+
+        static private void writeStringToFile(string jsonString)
+        {
+            string jsonFilePath = ConfigurationManager.AppSettings["jsonFilePath"];
+            Console.WriteLine("Ecriture du fichier JSON");
+            try
+            {
+                System.IO.File.WriteAllText(jsonFilePath, jsonString);
+            }
+            catch (IOException e)
+            {
+                throw e;
+            }
+            Console.WriteLine("Terminer");
         }
     }
 }
