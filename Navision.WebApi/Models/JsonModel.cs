384aff55489b54c0b818a0babf25e44e9f49b599
loriscalisti <loris.calisti@e-kenz.com>
2018-05-28 10:50:23 +0200

    Ajout de routes pour récupérer le fichier de config et les documents PDF

---
+++ Navision.WebApi/Models/JsonModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Navision.WebApi.Models
+{
+    public class JsonModel
+    {
+        public string Version { get; set; }
+        public List<Company> Companies { get; set; }
+    }
+
+    public class Company
+    {
+        public string CompanyName { get; set; }
+        public string Url { get; set; }
+        public string Domain { get; set; }
+    }
+}
---
+++ Navision.WebApi/Models/PdfModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Navision.WebApi.Models
+{
+    public class PdfModel
+    {
+        public string fileName { get; set; }
+        public byte[] fileStream { get; set; }
+    }
+}
