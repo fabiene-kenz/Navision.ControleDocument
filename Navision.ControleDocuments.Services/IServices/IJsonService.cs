80e66783236aa90d07bbe751ee155d96f53f1771
Fabien <fabien.richard@hotmail.fr>
2018-06-29 14:46:36 +0200

    add file

---
+++ Navision.ControleDocuments.Services/IServices/IJsonService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Navision.ControleDocuments.Services.IServices
+{
+   public interface IJsonService
+    {
+        string GetJson();
+    }
+}
---
+++ Navision.ControleDocuments.Services/Services/JsonService.cs
@@ -0,0 +1,30 @@
+﻿using Navision.ControleDocuments.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Navision.ControleDocuments.Services.Services
+{
+    public class JsonService : IJsonService
+    {
+        public string GetJson()
+        {
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    var arrayByteConfig = webClient.DownloadData("http://navapi.saas.e-kenz.com/Json/Config.json");
+                    System.Text.Encoding enc = System.Text.Encoding.ASCII;
+                    string configJson = enc.GetString(arrayByteConfig);
+                    return configJson;
+                }
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+            
+        }
+    }
+}
