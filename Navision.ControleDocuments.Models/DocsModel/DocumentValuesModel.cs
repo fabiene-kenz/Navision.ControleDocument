d184909723d48ddd976517bde3437e9bb0e3666c
Fabien <fabien.richard@hotmail.fr>
2018-07-02 11:45:41 +0200

    Update Solution

---
+++ Navision.ControleDocuments.Models/DocsModel/DocumentValuesModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Navision.ControleDocuments.Models.DocsModel
+{
+    public class DocumentValuesModel
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
